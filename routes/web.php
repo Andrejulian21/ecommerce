a97fe9d1ad0496d701a0e57f76f02e67eb216905
Andrejulian21 <julian21arteaga@gmail.com>
2025-09-09 13:54:37 -0500

    rutas basicas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    return 'List products';
+});
+
+Route::get('/products/{id}/{category?}', function ($id, $category = null) {
+    return 'Detail products' . $id . "with category: ". $category;
+});
