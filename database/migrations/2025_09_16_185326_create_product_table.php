b77adf1dd9cb69f980006f0043dc36d66301be57
Andrejulian21 <julian21arteaga@gmail.com>
2025-09-16 15:00:26 -0500

    migracion images_products

--- database/migrations/2025_09_16_185326_create_product_table.php
+++ database/migrations/2025_09_16_185326_create_product_table.php
@@ -6,20 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('product', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price');
+            $table->string('url_image');
+            $table->string('category');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('product');
